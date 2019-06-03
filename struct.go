18db5e17044fe46c6f10761e099feadb4f550ff5
woong <woong@lazypic.org>
2019-06-03 20:35:42 +0900

    바이트를 줄일

--- struct.go
+++ struct.go
@@ -16,38 +16,38 @@ type Hw struct {
 
 // Sw 는 소프트웨어를 관리하기 위한 자료구조이다.
 type Sw struct {
-	Typ                  string  // 타입
-	CreateDate           string  // 생성일
-	Sn                   string  // 시리얼넘버
-	Product              string  // 제품명
-	MonthMaintenanceCost float64 // 월유지비
-	MonetaryUnit         string  // 가격단위
-	Description          string  // 설명
+	Typ          string  // 타입
+	CreateDate   string  // 생성일
+	Sn           string  // 시리얼넘버
+	Product      string  // 제품명
+	MonthCost    float64 // 월유지비
+	MonetaryUnit string  // 가격단위
+	Description  string  // 설명
 }
 
 // Account 는 공용계정을 관리하기 위한 자료구조이다.
 type Account struct {
-	Typ                  string  // 타입
-	CreateDate           string  // 생성일
-	Product              string  // 제품명
-	ProductURL           string  // 주소
-	ID                   string  // ID
-	Password             string  // 패스워드
-	MonthMaintenanceCost float64 // 월유지비
-	MonetaryUnit         string  // 가격단위
-	PurchaseDate         string  // 구매일
-	Description          string  // 설명
+	Typ          string  // 타입
+	CreateDate   string  // 생성일
+	Product      string  // 제품명
+	ProductURL   string  // 주소
+	ID           string  // ID
+	Password     string  // 패스워드
+	MonthCost    float64 // 월유지비
+	MonetaryUnit string  // 가격단위
+	PurchaseDate string  // 구매일
+	Description  string  // 설명
 }
 
 // RealEstate 는 부동산을 관리하기 위한 자료구조이다.
 type RealEstate struct {
-	Typ                  string  // 타입
-	CreateDate           string  // 생성일
-	ContractDate         string  // 구매일
-	MonthMaintenanceCost float64 // 월유지비
-	MonetaryUnit         string  // 가격단위
-	Address              string  // 주소
-	Description          string  // 설명
+	Typ          string  // 타입
+	CreateDate   string  // 생성일
+	ContractDate string  // 구매일
+	MonthCost    float64 // 월유지비
+	MonetaryUnit string  // 가격단위
+	Address      string  // 주소
+	Description  string  // 설명
 }
 
 // Other 는 위 분류에 들어가지 않는 항목이다. 예) 안마의자
