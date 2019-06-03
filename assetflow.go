29febf67771372bee78f6a2867d775325d9c3385
kim hanwoong <khw7096@gmail.com>
2019-06-04 01:09:21 +0900

    자료구조를 좀더 손봄.

--- assetflow.go
+++ assetflow.go
@@ -8,14 +8,46 @@ import (
 )
 
 var (
-	flagAdd  = flag.Bool("add", false, "add a item")
-	flagType = flag.String("type", "", "item type")
+	// db setting
+
+	// mode
+	flagAdd    = flag.Bool("add", false, "add mode")
+	flagUpdate = flag.Bool("update", false, "update mode")
+	flagRm     = flag.Bool("rm", false, "rm mode")
+
+	// key
+	flagType       = flag.String("type", "", "item type")              // partition key
+	flagCreateDate = flag.String("createdate", "", "item create date") // sort key
+
+	// attributes
+	flagProduct        = flag.String("product", "", "product name")
+	flagProductStatus  = flag.String("productstatus", "", "product status")
+	flagUser           = flag.String("user", "", "product user name")
+	flagSn             = flag.String("sn", "", "product serial number")
+	flagCost           = flag.Float64("cost", 0.0, "product cost")
+	flagMonetaryUnit   = flag.String("monetaryunit", "KRW", "price monetary unit")
+	flagPurchaseDate   = flag.String("purchasedate", "", "product purchase date")
+	flagDescription    = flag.String("description", "", "description")
+	flagURL            = flag.String("url", "", "url")
+	flagID             = flag.String("id", "", "id")
+	flagPassword       = flag.String("password", "", "password")
+	flagContractDate   = flag.String("contractdate", "", "contract date")
+	flagAddress        = flag.String("address", "", "address")
+	flagMonthlyPayment = flag.Bool("monthlypayment", false, "monthly payment")
 )
 
 func usage() {
 	fmt.Fprintf(os.Stderr, "usage:\n")
 	fmt.Fprintf(os.Stderr, "\tassetflow -add ...")
 	os.Exit(2)
+
+	/*
+		assetflow -add -type hw -user woong -cost 6500000 -product imac
+		assetflow -add -type sw -cost 6500000 -product hwp
+		assetflow -add -type account -url https://test.com -id woong -pw test -cost 6500000 -monthlypayment
+		assetflow -add -type realestate -cost 6500000 -address 주소 -monthlypayment
+		assetflow -add -type other -product 안마의자 -cost 35000 -monthlypayment
+	*/
 }
 
 func main() {
--- struct.go
+++ struct.go
@@ -2,61 +2,66 @@ package main
 
 // Hw 는 데스크탑, 노트북등의 하드웨어 관리를 위한 자료구조이다.
 type Hw struct {
-	Typ           string  // 타입
-	CreateDate    string  // 생성일
-	Product       string  // 제품명
-	ProductStatus string  // 상태
-	ProductUser   string  // 사용자
-	Sn            string  // 시리얼넘버
-	Price         float64 // 가격
-	PurchaseDate  string  // 구매일
-	MonetaryUnit  string  // 가격단위
-	Description   string  // 설명
+	Typ            string  // 타입
+	CreateDate     string  // 생성일
+	Product        string  // 제품명
+	ProductStatus  string  // 상태
+	ProductUser    string  // 사용자
+	Sn             string  // 시리얼넘버
+	Cost           float64 // 가격
+	PurchaseDate   string  // 구매일
+	MonetaryUnit   string  // 가격단위
+	Description    string  // 설명
+	MonthlyPayment bool    // 월결제
 }
 
 // Sw 는 소프트웨어를 관리하기 위한 자료구조이다.
 type Sw struct {
-	Typ          string  // 타입
-	CreateDate   string  // 생성일
-	Sn           string  // 시리얼넘버
-	Product      string  // 제품명
-	MonthCost    float64 // 월유지비
-	MonetaryUnit string  // 가격단위
-	Description  string  // 설명
+	Typ            string  // 타입
+	CreateDate     string  // 생성일
+	Sn             string  // 시리얼넘버
+	Product        string  // 제품명
+	Cost           float64 // 유지비
+	MonetaryUnit   string  // 가격단위
+	Description    string  // 설명
+	MonthlyPayment bool    // 월결제
 }
 
 // Account 는 공용계정을 관리하기 위한 자료구조이다.
 type Account struct {
-	Typ          string  // 타입
-	CreateDate   string  // 생성일
-	Product      string  // 제품명
-	ProductURL   string  // 주소
-	ID           string  // ID
-	Password     string  // 패스워드
-	MonthCost    float64 // 월유지비
-	MonetaryUnit string  // 가격단위
-	PurchaseDate string  // 구매일
-	Description  string  // 설명
+	Typ            string  // 타입
+	CreateDate     string  // 생성일
+	Product        string  // 제품명
+	ProductURL     string  // 주소
+	ID             string  // ID
+	Password       string  // 패스워드
+	Cost           float64 // 월유지비
+	MonetaryUnit   string  // 가격단위
+	PurchaseDate   string  // 구매일
+	Description    string  // 설명
+	MonthlyPayment bool    // 월결제
 }
 
 // RealEstate 는 부동산을 관리하기 위한 자료구조이다.
 type RealEstate struct {
-	Typ          string  // 타입
-	CreateDate   string  // 생성일
-	ContractDate string  // 구매일
-	MonthCost    float64 // 월유지비
-	MonetaryUnit string  // 가격단위
-	Address      string  // 주소
-	Description  string  // 설명
+	Typ            string  // 타입
+	CreateDate     string  // 생성일
+	ContractDate   string  // 구매일
+	Cost           float64 // 월유지비
+	MonetaryUnit   string  // 가격단위
+	Address        string  // 주소
+	Description    string  // 설명
+	MonthlyPayment bool    // 월결제
 }
 
 // Other 는 위 분류에 들어가지 않는 항목이다. 예) 안마의자
 type Other struct {
-	Typ                  string  // 타입
-	CreateDate           string  // 생성일
-	PurchaseDate         string  // 구매일
-	Product              string  // 제품명
-	MonthMaintenanceCost float64 // 월유지비
-	MonetaryUnit         string  // 가격단위
-	Description          string  // 설명
+	Typ            string  // 타입
+	CreateDate     string  // 생성일
+	PurchaseDate   string  // 구매일
+	Product        string  // 제품명
+	Cost           float64 // 유지비
+	MonetaryUnit   string  // 가격단위
+	Description    string  // 설명
+	MonthlyPayment bool    // 월결제
 }
