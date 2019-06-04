aeb70b8c406d1f736c86779c73b0b35449cea6b9
kim hanwoong <khw7096@gmail.com>
2019-06-04 23:08:58 +0900

    필요한 자료구조를 추가함.

--- struct.go
+++ struct.go
@@ -16,6 +16,61 @@ type Hw struct {
 	MonthlyPayment bool   // 월결제
 }
 
+// Sound 는 사운드 장비 관리를 위한 자료구조이다.
+type Sound struct {
+	Typ           string // 타입
+	CreateDate    string // 생성일
+	Product       string // 제품명
+	ProductStatus string // 상태
+	ProductUser   string // 사용자
+	Cost          int64  // 가격
+	PurchaseDate  string // 구매일
+	MonetaryUnit  string // 가격단위
+	Description   string // 설명
+}
+
+// Camera 는 카메라 장비 관리를 위한 자료구조이다.
+type Camera struct {
+	Typ           string // 타입
+	CreateDate    string // 생성일
+	Product       string // 제품명
+	ProductStatus string // 상태
+	ProductUser   string // 사용자
+	Cost          int64  // 가격
+	PurchaseDate  string // 구매일
+	MonetaryUnit  string // 가격단위
+	Description   string // 설명
+}
+
+// Lens 는 카메라 장비 관리를 위한 자료구조이다.
+type Lens struct {
+	Typ           string // 타입
+	CreateDate    string // 생성일
+	Product       string // 제품명
+	ProductStatus string // 상태
+	ProductUser   string // 사용자
+	Cost          int64  // 가격
+	PurchaseDate  string // 구매일
+	MonetaryUnit  string // 가격단위
+	Description   string // 설명
+	Sn            string // 시리얼넘버
+	Angle         string // 화각
+}
+
+// Rig 는 카메라 리그장비 관리를 위한 자료구조이다.
+type Rig struct {
+	Typ           string // 타입
+	CreateDate    string // 생성일
+	Product       string // 제품명
+	ProductStatus string // 상태
+	ProductUser   string // 사용자
+	Cost          int64  // 가격
+	PurchaseDate  string // 구매일
+	MonetaryUnit  string // 가격단위
+	Description   string // 설명
+	Sn            string // 시리얼넘버
+}
+
 // Sw 는 소프트웨어를 관리하기 위한 자료구조이다.
 type Sw struct {
 	Typ            string // 타입
